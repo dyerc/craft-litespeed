1a714e332ffb66cfb5fa7e00e9b1352cf6ab840c
Chris Dyer <chris@cdyer.co.uk>
2024-06-14 14:22:16 +0100

    Test with Craft 3.

--- src/controllers/CacheController.php
+++ src/controllers/CacheController.php
@@ -11,7 +11,7 @@ use yii\web\Response;
 
 class CacheController extends Controller
 {
-    protected array|bool|int $allowAnonymous = true;
+    protected $allowAnonymous = true;
 
     public function actionClear(): Response
     {
